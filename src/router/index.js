2629f0e100db60d912a44be2841041bc9e4980ca
Mhmdmnsouri <Bitshawcoder@gmail.com>
2025-05-24 08:33:57 +0330

    Add : add-address route & Redirect '/' to add-address page

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "@/views/index.vue";
+import AddAddress from "@/views/AddAddress.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/add-address",
+      name: "address.add",
+      component: AddAddress,
+    },
+    {
       path: "/",
-      name: "home",
-      component: Home,
+      redirect: { name: "address.add" },
     },
   ],
 });
